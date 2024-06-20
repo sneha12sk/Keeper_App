197849ae795f00470eb665c49b928ae11c5cd92d
sneha12sk <sksneha73@gmail.com>
2024-06-21 01:37:24 +0530

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,5 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./components/App";
+import "bootstrap-icons/font/bootstrap-icons.css";
+ReactDOM.render(<App />, document.getElementById("root"));
